6522af967b7e490b2a5a6a2acb797d8ecac6cee8
boygalaxy1096@gmail.com <boygalaxy1096@gmail.com@c1895e98-c0f9-c2fa-a38c-251c0643b80a>
2014-11-18 15:56:44 +0000

    Mapeditor ver 1.0 Author Tan Gay

---
+++ trunk/MapEditor1/Object.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace MapEditor1
+{
+    public class Object
+    {
+        public int ID;
+
+        public int IDIMAGE;
+
+        public Rectangle rect;
+
+        public int type;
+
+        public Point position;
+
+        public Object(int id, int idImage,Rectangle rect, int type, Point pos)
+        {
+            this.ID = id;
+            this.rect = rect;
+            this.type = type;
+            this.position = pos;
+        }
+
+        public double getWidth()
+        {
+            return rect.Width;
+
+        }
+        public double getHeight()
+        {
+            return rect.Height;
+        }
+
+    }
+}
